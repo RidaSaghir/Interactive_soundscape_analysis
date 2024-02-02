6d47de2d8d6d9705328d1d222ef1fe8bb88da23d
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2024-02-02 15:07:44 +0100

    Changed server name for hugging face

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ class AcousticAnalyzerApp:
 
 
         if __name__ == "__main__":
-            demo.launch()
+            demo.launch(server_name='0.0.0.0', server_port=7860)
 
 app = AcousticAnalyzerApp()
 app.launch_app()
