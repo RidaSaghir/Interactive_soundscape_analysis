Edited to avoid re scaling of data in clustering leading in wrong clusters. Still have to fix the code for optimal clusters

clustering.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
-from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
 from scipy.cluster.hierarchy import linkage, dendrogram
+from pca import pca
 
 
 class ClusteringVisualizer:
@@ -19,9 +19,7 @@ class ClusteringVisualizer:
 
     def find_optimal_clusters(self, data, max_clusters):
         max_k = int(max_clusters)
-        # Standardize the data
-        scaler = StandardScaler()
-        self.scaled_data = scaler.fit_transform(data)
+
         # Initialize variables to store the best Silhouette score and the corresponding k
         best_score = -1
         best_k = 0
@@ -32,8 +30,8 @@ class ClusteringVisualizer:
 
         for k in k_values:
             kmeans = KMeans(n_clusters=k, n_init=10)
-            data.loc[:, 'Cluster'] = kmeans.fit_predict(self.scaled_data)
-            silhouette_avg = silhouette_score(self.scaled_data, data['Cluster'])
+            data.loc[:, 'Cluster'] = kmeans.fit_predict(data)
+            silhouette_avg = silhouette_score(data, data['Cluster'])
             silhouette_scores.append(silhouette_avg)
 
             # Check if the current k has a better Silhouette score
@@ -47,35 +45,18 @@ class ClusteringVisualizer:
 
         return best_k, fig
 
-    def pca(self, num_dim, clusters_ideal, num_clusters, max_clusters):
-        # Select only the columns with acoustic indices
-        selected_data = self.data[['ACI', 'ENT', 'EVN', 'LFC', 'MFC', 'HFC', 'EPS', 'EAS', 'ECV']]  # Add other indices as needed
 
-        # Standardize the data
-        scaler = StandardScaler()
-        self.scaled_data = scaler.fit_transform(selected_data)
-
-        # Perform PCA
-        pca = PCA(n_components=num_dim)  # Choose the number of components
-        principal_components = pca.fit_transform(self.scaled_data)
-        # Create a new DataFrame with the principal components
-        columns = [f'Principal Component {i + 1}' for i in range(principal_components.shape[1])]
-        self.df_pca = pd.DataFrame(data=principal_components, columns=columns)
-        optimal_clusters = 1
-        if clusters_ideal == 'Get optimum number of clusters':
-            self.optimal_clusters, self.sil_score = self.find_optimal_clusters(selected_data, max_clusters)
-        elif clusters_ideal == 'Choose the number of clusters':
-            self.optimal_clusters = num_clusters
-
-        # Concatenate the original DataFrame with the new DataFrame
-        result_df = pd.concat([self.data[['File', 'Timestamp']], self.df_pca], axis=1)
-        result_df.to_csv('result_df.csv')
-        return self.df_pca, selected_data, result_df, columns, self.optimal_clusters, self.sil_score
     def kmeans_clustering(self, clustering, num_dim, columns, clusters_ideal, num_clusters, max_clusters):
 
         if clustering == 'pca':
-            # This dict includes filename, timestamp, and PC columns
-            self.df_pca, selected_data, result_df, columns, self.optimal_clusters, self.sil_score = self.pca(num_dim, clusters_ideal, num_clusters, max_clusters)
+            # Data already scaled in PCA function
+            self.df_pca, result_df, columns = pca(self.data, num_dim, clusters_ideal, num_clusters, max_clusters)
+
+            if clusters_ideal == 'Get optimum number of clusters':
+                self.optimal_clusters, self.sil_score = self.find_optimal_clusters(self.df_pca, max_clusters)
+            elif clusters_ideal == 'Choose the number of clusters':
+                self.optimal_clusters = num_clusters
+
             # Apply K-Means clustering
             kmeans = KMeans(n_clusters=self.optimal_clusters)
             self.data['KMeans_Cluster'] = kmeans.fit_predict(self.df_pca)
@@ -84,30 +65,21 @@ class ClusteringVisualizer:
             # Remove existing PCA columns from the DataFrame
             existing_pca_columns = [col for col in self.data.columns if col.startswith('Principal Component')]
             self.data = self.data.drop(columns=existing_pca_columns, errors='ignore')
-            self.data.to_csv('self_data.csv')
-
-            # Concatenate the new PCA columns
+            # Concatenate the new PCA columns (the whole original dataframe concatenated with PCs)
             self.data = pd.concat([self.data, self.df_pca], axis=1)
 
-
-            # Populate the cluster members dictionary
-            for idx, row in self.data.iterrows():
-                cluster_label = row['KMeans_Cluster']
-                file_name = row['File']
-                cluster_members[cluster_label].append(file_name)
-            #self.data = pd.concat([self.data, df_pca], axis = 1)
 
         if clustering == 'acoustic':
             # Select the columns for clustering
             selected_data = self.data[columns]
-
             # Standardize the data
             scaler = StandardScaler()
             self.scaled_data = scaler.fit_transform(selected_data)
+            scaled_df = pd.DataFrame(self.scaled_data, columns=selected_data.columns)
 
-            optimal_clusters = 1
             if clusters_ideal == 'Get optimum number of clusters':
-                self.optimal_clusters, self.sil_score = self.find_optimal_clusters(selected_data, max_clusters)
+                print(self.scaled_data)
+                self.optimal_clusters, self.sil_score = self.find_optimal_clusters(scaled_df, max_clusters)
             elif clusters_ideal == 'Choose the number of clusters':
                 self.optimal_clusters = num_clusters
 
@@ -115,14 +87,13 @@ class ClusteringVisualizer:
             kmeans = KMeans(n_clusters=self.optimal_clusters)
             self.data['KMeans_Cluster'] = kmeans.fit_predict(self.scaled_data)
 
-            # Create a dictionary to store cluster members (file names)
-            cluster_members = {i: [] for i in range(self.optimal_clusters)}
-
-            # Populate the cluster members dictionary
-            for idx, row in self.data.iterrows():
-                cluster_label = row['KMeans_Cluster']
-                file_name = row['File']
-                cluster_members[cluster_label].append(file_name)
+        # Create a dictionary to store cluster members (file names)
+        cluster_members = {i: [] for i in range(self.optimal_clusters)}
+        # Populate the cluster members dictionary
+        for idx, row in self.data.iterrows():
+            cluster_label = row['KMeans_Cluster']
+            file_name = row['File']
+            cluster_members[cluster_label].append(file_name)
 
         # Plot the clusters
         colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple', 'pink', 'lime', 'brown', 'gray', 'indigo']
@@ -144,7 +115,6 @@ class ClusteringVisualizer:
         # Apply hierarchical clustering
         agg_cluster = AgglomerativeClustering(n_clusters=self.optimal_clusters, affinity='euclidean', linkage='ward')
         hierarchical_labels = agg_cluster.fit_predict(self.scaled_data)
-        print(self.scaled_data)
 
         if clustering == ('pca'):
             linkage_matrix = linkage(self.df_pca, method='ward')