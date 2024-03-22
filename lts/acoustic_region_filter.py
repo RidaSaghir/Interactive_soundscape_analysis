20d52e56f26bc91a3df2327e33f8f7449a77da2d
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2024-03-22 13:29:12 +0100

    changes to keep file name as index

--- lts/acoustic_region_filter.py
+++ lts/acoustic_region_filter.py
@@ -31,7 +31,7 @@ def region_filter(df, region):
     elif region in [17, 18, 19, 20]:
         filtered_df = df[(df['Date Time'].dt.time >= ref_4.time()) & (df['Date Time'].dt.time <= ref_5.time())]
 
-    # Resetting index so that the missing indexes do not cause problems ahead
+    filtered_df['File Name'] = filtered_df.index
     filtered_df = filtered_df.reset_index(drop=True)
     freq_ranges = {
         1: (0, 988), 5: (988, 3609), 9: (3609, 7906), 13: (7906, 11000),
@@ -48,14 +48,14 @@ def region_filter(df, region):
             indexes.append(i)
 
     # Filtering columns that have per_bin values
-    selected_cols = [col for col in filtered_df.columns if 'per_bin' in col or 'Date Time' in col]
+    selected_cols = [col for col in filtered_df.columns if 'per_bin' in col or 'Date Time' in col or 'File Name' in col]
     selected_cols.remove('AGI_per_bin')
     filtered_df = filtered_df[selected_cols]
     new_filtered_df = {}
     subset_indices = [[] for _ in range(len(filtered_df))]
 
     for col in selected_cols:
-        if col != 'Date Time':
+        if col != 'Date Time' and col != 'File Name':
             subset_indices = [[] for _ in range(len(filtered_df))]
             for x, value in enumerate(filtered_df[col]):
                 index_values = []
@@ -66,14 +66,19 @@ def region_filter(df, region):
             new_filtered_df[col] = subset_indices
         elif col == 'Date Time':
             new_filtered_df['Date'] = filtered_df[col]
+        elif col == 'File Name':
+            new_filtered_df['File Name'] = filtered_df[col]
+
 
     new_filtered_df = pd.DataFrame(new_filtered_df)
     selected_cols.remove('Date Time')
+    selected_cols.remove('File Name')
     for col in selected_cols:
         for x in range(len(new_filtered_df)):
             val = new_filtered_df.loc[x, col]
             value = sum(val) / len(val)
             new_filtered_df.loc[x, col] = value
+    new_filtered_df.set_index('File Name', inplace=True)
 
     return new_filtered_df, selected_cols
 
