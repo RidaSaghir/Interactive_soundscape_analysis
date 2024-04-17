ea3377816de7fcd3f7bef2c7c673c2f3f6e55517
rida saghir <ridasaghir@ridas-MacBook-Pro.local>
2024-04-17 12:39:52 +0200

    changes to include file names as a separate column so that repeating file names are not an issue. Also to handle nan values

--- lts/date_parser.py
+++ lts/date_parser.py
@@ -2,11 +2,25 @@ import re
 from datetime import datetime
 from multiprocessing import Pool
 import pandas as pd
+
+
 def parse_date(df):
+    """
+    Parses the date and rearranges the data frame suitable for next operations
+
+    Args:
+        df : Original data frame returned from MAAD scikit library for computing indices.
+    Returns:
+        df_with_dates : Data frame having additional column for 'Date Time' and 'File Name' with series index.
+    """
     with Pool() as pool:
         results = pool.map(_parse_date, df.iterrows())
 
     df_with_dates = pd.concat(results, axis=0)
+    df_with_dates.reset_index(drop=True, inplace=True)
+    df_with_dates.fillna(0, inplace=True)
+    # Deletes the following columns as they often result in infinity values
+    df_with_dates.drop(['AGI', 'AGI_per_bin'], axis=1, inplace=True)
     return df_with_dates
 
 def _parse_date(row):
@@ -16,9 +30,9 @@ def _parse_date(row):
     if match:
         year, month, day, hour, minute, second = match.groups()
         timestamp = datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
-        series['Date'] = timestamp
+        series['Date Time'] = timestamp
 
         # Create a DataFrame with a single row from the modified Series
     df_row = pd.DataFrame(series).transpose()
-
+    df_row['File Name'] = df_row.index
     return df_row
